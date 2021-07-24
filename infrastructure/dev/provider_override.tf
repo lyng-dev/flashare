7906460b1e3a89603eed1a40827c8069bfb77ef9
Steffen Mads Lyng <sm@lyng.dev>
2021-07-24 11:45:20 +0200

    added a bit more configuration

--- infrastructure/dev/provider_override.tf
+++ infrastructure/dev/provider_override.tf
@@ -1,6 +1,10 @@
 terraform {
   backend "s3" {
     bucket = "flashare-tfstate-dev" # CONFIG: Replace, Must be globally unique
-    dynamodb_table = "flashare-tfstate-lock"
+                                    # CONFIG: Must be created manually
+                                    # CONFIG: Create using ./setup-prepare-aws.sh
+    dynamodb_table = "flashare-tfstate-lock" # CONFIG: Replace, Must be
+                                             # CONFIG: Must be created manually
+                                             # CONFIG: Create using ./setup-prepare-aws.sh
   }
 }
